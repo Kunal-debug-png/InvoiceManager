60c310bb0b2a3bfc07b6cce5fa0a9f3672fdcb57
Kunal Chhablani <kunalchhablani14@gmail.com>
2024-07-10 23:38:28 +0530

    Initial Commit

---
+++ InvoiceManager/InvoiceManager/src/test/java/com/kunal/InvoiceManager/InvoiceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kunal.InvoiceManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
